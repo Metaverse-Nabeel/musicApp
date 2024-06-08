92b2a888e1f34adbbea893ab5e0c7c9e2dea61f6
Nabeel Ahmed <nabeelahmed3129@gmail.com>
2024-06-08 20:29:59 +0500

    Music App complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import type { PropsWithChildren } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
+  ActivityIndicator,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -9,23 +9,43 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
+import { setupPlayer, addTrack } from '../musicPlayerServices';
+import MusicPlayer from './screens/MusicPlayer';
 
 function App(): React.JSX.Element {
+  const [isPlayerReady, setIsPlayerReady] = useState(false)
 
+  async function setup() {
+    let isSetup = await setupPlayer();
+    if (isSetup) {
+      await addTrack()
+    }
+    setIsPlayerReady(isSetup);
+  }
+
+  useEffect(() => {
+    setup();
+  }, [])
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    )
+  }
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <View>
-        <Text>
-          Music App
-        </Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <StatusBar barStyle={'light-content'}/>
+      <MusicPlayer/>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
 
 });
 
